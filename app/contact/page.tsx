85cd1730c94527777ec77e1dba3434b4d0562fcc
samev <samevangelista07@icloud.com>
2025-12-07 15:52:11 -0600

    Basic Routing

---
+++ app/contact/page.tsx
@@ -0,0 +1,51 @@
+export default function Contact() {
+    return(
+        <div>
+            Contact Us
+            <div className="flex">
+                <p>
+                For corporate contacting, email:
+                exec.shpe.uiuc@gmail.com
+
+                For on-campus contacting, email:
+                shpe.uiuc.vp.internal@gmail.com
+
+                For financial-related affairs, email:
+                treasurer.shpe.uiuc@gmail.com
+
+                For social events and programs, email:
+                shpe.uiuc.membership@gmail.com
+
+                For technical projects and workshops, email:
+                shpe.uiuc.technicalchair@gmail.com
+
+                For office administration, email:
+                shpe.uiuc.secretary@gmail.com
+                </p>
+                <p>
+                    For graduate and research affairs, email:
+                    shpe.uiuc.ga@gmail.com
+
+                    For alumni affairs, email:
+                    student.advisor.shpe.uiuc@gmail.com
+
+                    For community service and volunteering, email:
+                    shpe.uiuc.service@gmail.com
+
+                    For high school outreach, email:
+                    shpe.uiuc.outreach@gmail.com
+
+                    For marketing affairs, email:
+                    shpe.uiuc.marketing@gmail.com
+
+                    For academic affairs, email:
+                    shpe.uiuc.academic@gmail.com
+
+
+                </p>
+            </div>
+            
+            
+        </div>
+    );
+}
