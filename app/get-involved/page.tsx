cfafaecf53ec07a8b4f9976354eaf3ac53b29ac7
Kevin <kcruz28@illinois.edu>
2025-12-10 00:40:02 -0600

    added image to get involved page

--- app/get-involved/page.tsx
+++ app/get-involved/page.tsx
@@ -84,14 +84,32 @@ export default function GetInvolved() {
     return (
         <div className="bg-white min-h-screen">
             {/* Header */}
-            <div className="bg-gray-50 py-16 sm:py-24">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-                    <h1 className="text-4xl font-extrabold text-shpe-blue sm:text-5xl sm:tracking-tight lg:text-6xl">
-                        Get Involved
-                    </h1>
-                    <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-                        Join the familia and kickstart your professional journey. Click on a committee to learn more.
-                    </p>
+            <div className="relative bg-gray-50 overflow-hidden">
+                <div className="max-w-7xl mx-auto">
+                    <div className="relative z-10 pb-8 bg-gray-50 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                        <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-gray-50 transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+                            <polygon points="50,0 100,0 50,100 0,100" />
+                        </svg>
+
+                        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                            <div className="sm:text-center lg:text-left">
+                                <h1 className="text-4xl tracking-tight font-extrabold text-shpe-blue sm:text-5xl md:text-6xl">
+                                    <span className="block xl:inline">Get Involved with</span>{' '}
+                                    <span className="block text-shpe-orange xl:inline">SHPE UIUC</span>
+                                </h1>
+                                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                    Join the familia and kickstart your professional journey. Click on a committee below to learn more about how you can participate and grow.
+                                </p>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+                <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <img
+                        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                        src="/get_involed.JPG"
+                        alt="SHPE Members getting involved"
+                    />
                 </div>
             </div>
 
