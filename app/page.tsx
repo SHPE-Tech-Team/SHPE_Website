0b17f26191779c640a374a7a1ac0d3c831512536
heidis4 <heidi.solis@illinois.edu>
2026-02-22 15:35:59 -0600

    New events container

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import HeroCarousel from "./components/HeroCarousel";
+import EventHighlights from "./components/EventHighlights";
 import SectionHeader from "./components/SectionHeader";
 import PillarCard from "./components/PillarCard";
 import InfoCard from "./components/InfoCard";
@@ -19,6 +20,91 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       {/* Hero Section */}
       <HeroCarousel />
+
+
+      <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8 mt-8 flex gap-8 items-start">
+        {/* Stay Connected Section */}
+        <section className="flex-1 py-8 bg-gray-50 w-full relative overflow-hidden rounded-lg">
+          {/* Background Blob */}
+          <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0 pointer-events-none">
+            <div className="absolute -top-[20%] -right-[10%] w-[50%] h-[80%] bg-blue-100/40 rounded-full blur-3xl opacity-60"></div>
+            <div className="absolute top-[30%] -left-[10%] w-[40%] h-[60%] bg-orange-100/40 rounded-full blur-3xl opacity-60"></div>
+          </div>
+
+          <div className="max-w-6xl mx-auto px-4 sm:px-4 lg:px-6 relative z-10">
+            <FadeIn>
+              <SectionHeader
+                title="Stay connected with SHPE"
+              />
+            </FadeIn>
+
+            <div className="flex flex-col gap-4">
+              {/* Newsletter */}
+              <FadeIn delay={0.1} className="h-full">
+                <InfoCard
+                  horizontal
+                  title="Linktree"
+                  description="Our central hub for all SHPE links, sign-up forms, and the monthly newsletter! Check it out to stay in the loop."
+                  icon={
+                    <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+                  }
+                  link={{
+                    text: "View Linktree",
+                    href: "https://linktr.ee/SHPE.UI?utm_source=linktree_profile_share&ltsid=94fd3c15-a042-4e7c-894a-31c020045687",
+                    target: "_blank"
+                  }}
+                />
+              </FadeIn>
+
+              {/* Slack */}
+              <FadeIn delay={0.2} className="h-full">
+                <InfoCard
+                  horizontal
+                  title="Slack"
+                  description="SHPE @ UIUC have a slack for members and perspective members to join! Communicate with other members and stay updated! Become a member of our slack!"
+                  iconColorClass="text-purple-600"
+                  iconBgColorClass="bg-purple-100"
+                  icon={
+                    <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24"><path d="M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.52 2.52 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.527 2.527 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.52v-6.315zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.52v2.522h-2.521zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V0a2.527 2.527 0 0 1 2.52-2.52h.003c1.393 0 2.52 1.127 2.52 2.52v6.314zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.521A2.527 2.527 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.52h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z" /></svg>
+                  }
+                  link={{
+                    text: "Join Slack",
+                    href: "https://join.slack.com/t/shpe-uiuc/shared_invite/zt-3bb1v0tpc-nmf3p9VJTEpLtX1tjb~iBw",
+                    target: "_blank"
+                  }}
+                />
+              </FadeIn>
+
+              {/* Instagram */}
+              <FadeIn delay={0.3} className="h-full">
+                <InfoCard
+                  horizontal
+                  title="Instagram"
+                  description="Follow @shpe.uiuc for event recaps, member spotlights, and daily updates."
+                  icon={
+                    <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.468 2.37c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clipRule="evenodd" /></svg>
+                  }
+                  iconBgColorClass="bg-pink-100"
+                  iconColorClass="text-pink-600"
+                  link={{
+                    text: "Follow @shpe_uiuc",
+                    href: "https://www.instagram.com/shpe_uiuc/?hl=en",
+                    target: "_blank"
+                  }}
+                />
+              </FadeIn>
+            </div>
+          </div>
+        </section>
+
+        {/* Event Highlights on the right */}
+        <div className="flex-1 py-8 bg-gray-50 w-full relative overflow-hidden rounded-lg">
+          <div className="h-full w-full bg-black">
+            <EventHighlights />
+          </div>
+        </div>
+      </div>
+
 
       {/* Mission & Vision Section */}
       <section className="pt-24 pb-12 bg-white w-full overflow-hidden">
@@ -78,78 +164,6 @@ export default function Home() {
                 />
               </FadeIn>
             ))}
-          </div>
-        </div>
-      </section>
-
-      {/* Stay Connected Section */}
-      <section className="py-20 bg-gray-50 w-full relative overflow-hidden">
-        {/* Background Blob */}
-        <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0 pointer-events-none">
-          <div className="absolute -top-[20%] -right-[10%] w-[50%] h-[80%] bg-blue-100/40 rounded-full blur-3xl opacity-60"></div>
-          <div className="absolute top-[30%] -left-[10%] w-[40%] h-[60%] bg-orange-100/40 rounded-full blur-3xl opacity-60"></div>
-        </div>
-
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
-          <FadeIn>
-            <SectionHeader
-              title="Stay connected with SHPE"
-              description="We offer many ways to stay connected on upcoming events, monthly general meetings, and any related SHPE-Chapter news."
-            />
-          </FadeIn>
-
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            {/* Newsletter */}
-            <FadeIn delay={0.1} className="h-full">
-              <InfoCard
-                title="Linktree"
-                description="Our central hub for all SHPE links, sign-up forms, and the monthly newsletter! Check it out to stay in the loop."
-                icon={
-                  <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
-                }
-                link={{
-                  text: "View Linktree",
-                  href: "https://linktr.ee/SHPE.UI?utm_source=linktree_profile_share&ltsid=94fd3c15-a042-4e7c-894a-31c020045687",
-                  target: "_blank"
-                }}
-              />
-            </FadeIn>
-
-            {/* Slack */}
-            <FadeIn delay={0.2} className="h-full">
-              <InfoCard
-                title="Slack"
-                description="SHPE @ UIUC have a slack for members and perspective members to join! Communicate with other members and stay updated! Become a member of our slack!"
-                iconColorClass="text-purple-600"
-                iconBgColorClass="bg-purple-100"
-                icon={
-                  <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24"><path d="M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.52 2.52 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.527 2.527 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.52v-6.315zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.52v2.522h-2.521zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V0a2.527 2.527 0 0 1 2.52-2.52h.003c1.393 0 2.52 1.127 2.52 2.52v6.314zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.521A2.527 2.527 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.52h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z" /></svg>
-                }
-                link={{
-                  text: "Join Slack",
-                  href: "https://join.slack.com/t/shpe-uiuc/shared_invite/zt-3bb1v0tpc-nmf3p9VJTEpLtX1tjb~iBw",
-                  target: "_blank"
-                }}
-              />
-            </FadeIn>
-
-            {/* Instagram */}
-            <FadeIn delay={0.3} className="h-full">
-              <InfoCard
-                title="Instagram"
-                description="Follow @shpe.uiuc for event recaps, member spotlights, and daily updates."
-                icon={
-                  <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.468 2.37c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clipRule="evenodd" /></svg>
-                }
-                iconBgColorClass="bg-pink-100"
-                iconColorClass="text-pink-600"
-                link={{
-                  text: "Follow @shpe_uiuc",
-                  href: "https://www.instagram.com/shpe_uiuc/?hl=en",
-                  target: "_blank"
-                }}
-              />
-            </FadeIn>
           </div>
         </div>
       </section>
